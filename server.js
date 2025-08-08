74c0c921a47546e1bec572ffb98f8d4220066a49
RemiFabre <remifabre1800@gmail.com>
2025-08-08 16:06:45 +0200

    Updated model version in server.js too

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ server.get("/token", async () => {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      model: "gpt-4o-realtime-preview-2024-12-17",
+      model: "gpt-4o-realtime-preview-2025-06-03",
       voice: "verse",
     }),
   });
